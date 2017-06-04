c42d746b52d076e949ef9562838761671daf7600
Jirayut Leeupathumvong <jirayut_32547@hotmail.com>
2017-06-04 22:50:34 +0700

    move load data to seperate class with observer

--- app/src/main/java/com/softspec/transights/transights/MainActivity.java
+++ app/src/main/java/com/softspec/transights/transights/MainActivity.java
@@ -7,31 +7,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
-
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener{
 
-    private DatabaseReference mDatabase;
-
-    private ArrayList<Place> mPlace = new ArrayList<>();
-
-    private ArrayList<Station> stationList  = new ArrayList<>();
 
     private RecyclerView mPlaceLists;
     private RecyclerAdapter recyclerAdapter;
 
     private MenuItem searchItem, locationItem;
+
+    private TransightsPresenter transightsPresenter;
+    private RemoteDataRepository remoteDataRepository;
 
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -54,7 +44,6 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d("RUN", "ON CREATE");
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -63,103 +52,18 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
 //        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
 //        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
+        initRecycleView();
+    }
+
+    public void initRecycleView(){
+
         mPlaceLists = (RecyclerView) findViewById(R.id.place_recycleView);
         mPlaceLists.setHasFixedSize(true);
         mPlaceLists.setLayoutManager(new LinearLayoutManager(this));
 
-        recyclerAdapter = new RecyclerAdapter(mPlace, stationList, this);
-        mPlaceLists.setAdapter(recyclerAdapter);
-
-        addChildStation();
-        addChildPlace();
-
-    }
-
-    private void addChildStation(){
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("station");
-        mDatabase.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                loadStation(dataSnapshot);
-                recyclerAdapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-    }
-
-    private void addChildPlace(){
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("places");
-        mDatabase.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                loadPlace(dataSnapshot);
-                recyclerAdapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-    }
-
-    private void loadStation(DataSnapshot dataSnapshot){
-        String stationName = dataSnapshot.child("stationName").getValue(String.class);
-        stationList.add(new Station(dataSnapshot.child("stationName").getValue(String.class), 0, 0));
-    }
-
-    private void loadPlace(DataSnapshot dataSnapshot){
-        String stationName = dataSnapshot.child("stationName").getValue(String.class);
-        String placeID = dataSnapshot.child("placeID").getValue(String.class);
-        String placeName = dataSnapshot.child("placeName").getValue(String.class);
-        String description = dataSnapshot.child("description").getValue(String.class);
-        String imgSrc = dataSnapshot.child("imgSrc").getValue(String.class);
-
-        Place p = new Place(placeID, placeName, description, imgSrc);
-
-        mPlace.add(p);
-
-        for(Station s : stationList)
-            if(s.getStationName().equalsIgnoreCase(stationName))
-                s.addPlace(p);
-    }
-
-    private void loadTimeAndPrice(DataSnapshot dataSnapshot){
-        DataSnapshot tmpDataSnapshot = dataSnapshot.child("priceandtime");
+        remoteDataRepository = RemoteDataRepository.getInstance();
+        transightsPresenter = new TransightsPresenter(remoteDataRepository, this);
+        transightsPresenter.initialize();
     }
 
     @Override
@@ -188,6 +92,11 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
     public boolean onQueryTextChange(String newText) {
         search(newText);
         return true;
+    }
+
+    public void setPlaceList(ArrayList<Place> placeList, ArrayList<Station> stationList){
+        recyclerAdapter = new RecyclerAdapter(placeList, stationList, this);
+        mPlaceLists.setAdapter(recyclerAdapter);
     }
 
     public void search(String newText){
